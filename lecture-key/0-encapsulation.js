8b283f6ad82d7af1281cd1117bfb3160f98cd8fe
gonzalorportfolio <gromeroeducation@gmail.com>
2024-02-01 09:30:36 -0500

    update

--- lecture-key/0-encapsulation.js
+++ lecture-key/0-encapsulation.js
@@ -1,3 +1,12 @@
 const gonzaloShoeZero = {
-  colors: []
+  shoes: [],
+  addShoe(brand, model, size, colors ){
+    this.shoes.push({
+      id,
+      brand,
+      model,
+      size,
+      colors,
+    })
+}
 }
